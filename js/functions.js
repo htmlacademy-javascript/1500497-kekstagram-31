e489ab0dcef6e044ef2b47396f9a908260f0227c
RessourdAlex <pichkuryra@gmail.com>
2024-04-03 18:41:49 +0300

    fix task2

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,5 @@
 function checkStrLength(enterString, maxLenStr){ // Функция проверки длины строки
-  let result = enterString.length <= maxLenStr ? true : false;
+  const result = enterString.length <= maxLenStr ? true : false;
   return result;
 }
 
@@ -19,7 +19,7 @@ function numberSelection(arrayElement) { // Функция извлечения 
 
   for (let i = 0; i < arrayElement.length; i++){
     if (Number.isNaN(parseInt(arrayElement[i], 10)) === false){
-      strNumber += arrayElement[i]
+      strNumber += arrayElement[i];
     }
   }
 
@@ -31,23 +31,23 @@ let startStr = 'shrsdmdfdf';
 const arrayOfStrings = ['qweewq', 'fhjfgdfghdg', 'frthrnfvc', '', 'дов0д', '11134111', '4444444', 'наворован', 'asd hj j hds A'];
 const arrayOfStrNumbers = ['gbset464hj453', ' 2024 год',' ECMAScript 2022', 'grthgrhterge', '385 бегемотов весят 767 тонн', 123];
 
-console.log('Тестирование функции номер 1\n');
-console.log('Изначальная строка: ', startStr);
+// console.log('Тестирование функции номер 1\n');
+// console.log('Изначальная строка: ', startStr);
 for (let i = 0; i <= 5; i++) {
   console.log(checkStrLength(startStr, 18));
   startStr += 'qw';
 }
 
-console.log('\nТестирование функции номер 2\n');
-console.log('Набор тестируемых строк: ', arrayOfStrings);
+// console.log('\nТестирование функции номер 2\n');
+// console.log('Набор тестируемых строк: ', arrayOfStrings);
 arrayOfStrings.forEach((str, number) => {
   const result = PalindromYesNo((str.replaceAll(' ','')).toLowerCase());
   console.log(`Строка под номером ${number} ${result}`);
 }
 );
 
-console.log('\nТестирование функции номер 3\n');
-console.log('Набор тестируемых строк: ', arrayOfStrNumbers);
+// console.log('\nТестирование функции номер 3\n');
+// console.log('Набор тестируемых строк: ', arrayOfStrNumbers);
 arrayOfStrNumbers.forEach((str, number) => {
   const result = numberSelection(str);
   console.log(`${arrayOfStrNumbers[number]} : ${result}`);
